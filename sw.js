a26d98c5d47a141f67bcb3afe56a7a477244e2fa
Gabriel <gabibola955@gmail.com>
2026-05-26 14:46:32 +0100

    Modified service worker for better simplicity

--- sw.js
+++ sw.js
@@ -1,44 +1,16 @@
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.open("cache").then((cache) => {
-      return cache.match(event.request).then((response) => {
-        console.log("cache request: " + event.request.url);
-        const fetchPromise = fetch(event.request).then(
-          (networkResponse) => {
-            console.log(
-              "fetch completed: " + event.request.url,
-              networkResponse,
-            );
-            if (networkResponse) {
-              console.debug(
-                "updated cached page: " + event.request.url,
-                networkResponse,
-              );
-              cache.put(event.request, networkResponse.clone());
-            }
-            return networkResponse;
-          },
-          (event) => {
-            console.log("Error in fetch()", event);
-            event.waitUntil(
-              caches.open("cache").then((cache) => {
-                return cache.addAll([
-                  "./index.html",
-                  "./css/style.css",
-                  "./public/*",
-                  "./manifest.json",
-                  "https://platform.twitter.com/widgets.js",
-                ]);
-              }),
-            );
-          },
-        );
-        return response || fetchPromise;
-      });
-    }),
-  );
+const CACHE = "v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/script.js",
+  "/js/codes.js",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
-self.addEventListener("install", () => {
-  self.skipWaiting();
-  console.log("Latest version installed!");
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });

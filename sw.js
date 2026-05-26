dd790fb626ace32f947333b3b90d3131f59520fe
Gabriel <gabibola955@gmail.com>
2026-05-26 14:50:57 +0100

    Added activate listener to clear old caches

--- sw.js
+++ sw.js
@@ -14,3 +14,15 @@ self.addEventListener("install", (e) => {
 self.addEventListener("fetch", (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)),
+        ),
+      ),
+  );
+});
